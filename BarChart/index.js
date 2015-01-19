14dfb70d889248884cbc016218ff0a610f3e9087
thmain <tutorialhorizon@gmail.com>
2015-01-19 11:23:29 -0800

    refactored the attr functions to use object syntax

--- BarChart/index.js
+++ BarChart/index.js
@@ -300,6 +300,96 @@ function fullBarChart() {
     .call(axis.y); // Insert an axis inside this node
 }
 
+function fullBarChartRefactored() {
+  var maxHeight = 100;
+  var maxWidth = 400;
+  var barPadding = 0.01;
+  var margins = {
+    left: 20,
+    top: 20
+  };
+
+  // Define your conversion functions
+  var convert = {
+    x: d3.scale.ordinal(),
+    y: d3.scale.linear()
+  };
+
+  // Define the output range of your conversion functions
+  convert.y.range([maxHeight, 0]);
+  convert.x.rangeRoundBands([0, maxWidth], barPadding);
+
+  // Define your axis
+  var axis = {
+    x: d3.svg.axis().orient('bottom'),
+    y: d3.svg.axis().orient('left')
+  };
+  // Define the conversion function for the axis points
+  axis.x.scale(convert.x);
+  axis.y.scale(convert.y);
+
+  // Data: Average age of employees in an organization
+  var employees = [
+    {dept: 'A', age : 22},
+    {dept: 'B', age : 26},
+    {dept: 'C', age : 35},
+    {dept: 'D', age : 30},
+    {dept: 'E', age : 27}
+  ];
+
+  // Once you get your data, compute the domains
+  convert.x.domain(employees.map(function (d) {
+      return d.dept;
+    })
+  );
+  convert.y.domain([0, maxHeight]);
+
+  var chart = d3.select('.chart')
+    .attr({width: maxWidth})
+    .append('g')
+    .attr({
+        transform: function (d, i) {
+          return 'translate(' + margins.left + ',' + margins.top + ')';
+        }
+      });
+
+  var bars = chart
+    .selectAll('g')
+    .data(employees)
+    .enter()
+    .append('g') // Container for the each bar
+    .attr({
+      transform: function (d, i) {
+          return 'translate(' + convert.x(d.dept) + ', 0)';
+        }
+      });
+
+  bars.append('rect')
+    .attr({
+      y: function (d, i) {
+        return convert.y(d.age);
+      },
+      width: convert.x.rangeBand(),
+      height: function (d, i) {
+        return maxHeight - convert.y(d.age);
+      }
+    })
+
+  chart.append('g') // Container for the axis
+    .attr({
+      class: 'x axis',
+      transform: 'translate(0,' + maxHeight + ')'
+    })
+    .call(axis.x); // Insert an axis inside this node
+
+  chart.append('g') // Container for the axis
+    .attr({
+      class: 'y axis',
+      height: maxHeight
+    })
+    .call(axis.y); // Insert an axis inside this node
+}
+
 // d3CorrectBars();
 // d3InvertedBars();
 // d3CorrectSpacedBars();
@@ -307,4 +397,6 @@ function fullBarChart() {
 // nestedData();
 
 // chartWithAxis();
-fullBarChart();
+// fullBarChart();
+
+fullBarChartRefactored();
